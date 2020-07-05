37edcaf2c5673122ce4d25063f41fd21b0af266c
Nick Palenchar <nickpal@nickpalenchar.com>
2020-07-05 11:16:21 -0400

    top-level error handling dispalys github issue link

--- flipswitch/index.js
+++ flipswitch/index.js
@@ -3,6 +3,7 @@
 const chalk = require('chalk');
 
 const screens = require('./views');
+const {GITHUB_ISSUE_NEW_URL} = require('./configs.json');
 const {configureSingleRepo} = require("./views/configureRepoSearch");
 const {updateBranches} = require("./views/updateBranches");
 const {welcome} = require("./views/welcome");
@@ -15,6 +16,8 @@ const viewController = new CliController(0);
 viewController.run(welcome)
 .then(data => console.log('goodbye'))
     .catch(err => {
-        console.log('it didnt quite work', err);
+        console.log('An unexpected error!', err);
+        console.log('Please consider opening an issue and include as much info as nescessary (stacktrace, OS, actions you took, last screen you were on)')
+        console.log(GITHUB_ISSUE_NEW_URL);
         process.exit(err.code || 1);
     });
